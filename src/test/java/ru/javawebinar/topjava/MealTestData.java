9918fff4411b9941f0aa23590465708f82320cb5
afarida <fahiyarova@mail.ru>
2016-09-28 11:37:04 +0500

    MealTestData

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -3,12 +3,30 @@ package ru.javawebinar.topjava;
 import ru.javawebinar.topjava.matcher.ModelMatcher;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import static ru.javawebinar.topjava.model.BaseEntity.START_SEQ;
+
 /**
  * GKislin
  * 13.03.2015.
  */
 public class MealTestData {
 
-    public static final ModelMatcher<Meal> MATCHER = new ModelMatcher<>();
+    Map<Integer, Meal> meals = new HashMap<>();
+    {
+        meals.put(START_SEQ + 4, new Meal(START_SEQ + 4, LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500));
+        meals.put(START_SEQ + 5, new Meal(START_SEQ + 5, LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000));
+        meals.put(START_SEQ + 6, new Meal(START_SEQ + 6, LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500));
+        meals.put(START_SEQ + 7, new Meal(START_SEQ + 7, LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000));
+        meals.put(START_SEQ + 8, new Meal(START_SEQ + 8, LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500));
+        meals.put(START_SEQ + 9, new Meal(START_SEQ + 9, LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510));
+    }
 
+    public static final ModelMatcher<Meal> MATCHER = new ModelMatcher<>(
+            (expected, actual)-> Objects.equals(expected.toString(), actual.toString()));
 }
